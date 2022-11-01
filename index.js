ba9491fd8250ee8d09e2cfbab4a2fad364137dca
AbigailLabanok <abigail.labanok@gmail.com>
2022-11-01 13:16:13 -0400

    weeee

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const arr = [{ name: `Cass`, age: 15, mood: happy }];
+const arr = [
+  { name: `Cass`, age: 15, mood: `happy`, favNumbers: [1, 2, 3, 4] },
+  { name: `Abby`, age: 14, mood: `happy`, favNumbers: [2, 3, 4, 5] },
+  { name: `Oracle`, age: 15, mood: `sad`, favNumbers: [3, 4, 5, 6] },
+  { name: `LaPointe`, age: 17, mood: `happy`, favNumbers: [4, 5, 6, 7] },
+];
+
+//Part 1 & 2
+
+arr.forEach((person) => {
+  console.log(person.name);
+  person.favNumbers.forEach((number) => {
+    console.log(number);
+  });
+});
+
+//Part 3
+
+const filter = arr.filter((person) => person.mood === `happy`);
+console.log(filter);
